9bd2818a42eee01aa1fe2f8045ea5a0aa2bc0702
daknis <daknis@microsoft.com>
2022-02-01 10:40:42 -0800

    v1.2.1

--- src/msdo-client.ts
+++ src/msdo-client.ts
@@ -89,6 +89,10 @@ export class MsdoClient {
 
             args.push('--export-breaking-results-to-file');
             args.push(`${sarifFile}`);
+
+            args.push('--environment-name');
+            args.push('github');
+
         } catch (error) {
             error('Exception occurred while initializing MSDO:');
             error(error);
